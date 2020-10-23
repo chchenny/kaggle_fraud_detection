7c62198eb609c5eed64cf9ae9a4f43af247e20c7
cheryl c <cheryl.chen@turner.com>
2020-10-23 17:28:39 -0400

    formatting and linting

--- src/kaggle_credit_card_fraud_detection.py
+++ src/kaggle_credit_card_fraud_detection.py
@@ -1,48 +1,52 @@
+'''
+    Model Training
+'''
 from pathlib import Path
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import roc_auc_score, precision_score, recall_score, f1_score
+from sklearn.metrics import roc_auc_score, recall_score
 
 
 if __name__ == "__main__":
     current_dir = Path.cwd()
     data_dir = current_dir.joinpath("data")
     data_file = data_dir.joinpath("creditcard.csv")
-    random_seed = 111
-    try data_file.exists():
-        raw_df = pd.read_csv(data_file)
+    RANDOM_SEED = 111
+    assert data_file.exists()
+    raw_df = pd.read_csv(data_file)
     y = raw_df["Class"]
     X = raw_df.drop("Class", axis=1)
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.25, random_state=random_seed, stratify=y
+        X, y, test_size=0.25, random_state=RANDOM_SEED, stratify=y
     )
 
     clf = GaussianNB()
     clf.fit(X_train, y_train)
-    nb_auc = roc_auc_score(y_test, clf.predict_proba(X_test)[:,1])
+    nb_auc = roc_auc_score(y_test, clf.predict_proba(X_test)[:, 1])
     nb_recall = recall_score(y_test, clf.predict(X_test))
 
-    clf_rf = RandomForestClassifier(random_state=random_seed)
+    clf_rf = RandomForestClassifier(random_state=RANDOM_SEED)
     params = {
         "n_estimators": [30, 100, 500],
         "max_features": ["auto", "sqrt", "log2"],
-        "max_depth": range(4,10),
-        "criterion": ["gini", "entropy"]
+        "max_depth": range(4, 10),
+        "criterion": ["gini", "entropy"],
     }
 
     grid_clf = GridSearchCV(estimator=clf_rf, param_grid=params, cv=3)
     grid_clf.fit(X_train, y_train)
     print(grid_clf.best_params_)
     best_rf = grid_clf.best_estimator_
-    rf_auc = roc_auc_score(y_test, clf.predict_proba(X_test)[:,1])
+    rf_auc = roc_auc_score(y_test, clf.predict_proba(X_test)[:, 1])
     rf_recall = recall_score(y_test, clf.predict(X_test))
 
-    print(f"""
+    print(
+        f"""
         Naive Bayes result:
         \tAUC score: {nb_auc}\tRecall: {nb_recall}
         \tAUC score: {rf_auc}\tRecall: {rf_recall}
-    """)
+    """
+    )
