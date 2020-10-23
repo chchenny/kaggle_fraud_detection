19f0fe6fdeb2c0362e13dc469282a820d5cec2f8
cheryl c <cheryl.chen@turner.com>
2020-10-23 14:50:21 -0400

    More EDA & model training script

--- src/kaggle_credit_card_fraud_detection.py
+++ src/kaggle_credit_card_fraud_detection.py
@@ -1,9 +1,38 @@
+import pathlib
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import roc_auc_score, precision_score, recall_score, f1_score
+
+
+if __name__ == "__main__":
+    current_dir = pathlib.Path.cwd()
+    data_dir = current_dir.joinpath("data")
+    data_file = data_dir.joinpath("creditcard.csv")
+    random_seed = 111
+    try data_file.exists():
+        raw_df = pd.read_csv(data_file)
+    y = raw_df["Class"]
+    X = raw_df.drop("Class", axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=random_seed, stratify=y
+    )
+
+    clf = GaussianNB()
+    clf.fit(X_train, y_train)
+    roc_auc_score(y_test, clf.predict_proba(X_test)[:,1])
+    recall_score(y_test, clf.predict(X_test))
+
+    clf_rf = RandomForestClassifier(random_state=random_seed)
+    params = {
+        "n_estimators": [30, 100, 500],
+        "max_features": ["auto", "sqrt", "log2"],
+        "max_depth": range(4,10),
+        "criterion": ["gini", "entropy"]
+    }
+
+    grid_clf = GridSearchCV(estimator=clf_rf, param_grid=params, cv=3)
+    grid_clf.fit(X_train, y_train)
